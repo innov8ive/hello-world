e1b4e302f06fedbf248247841f6cf1df5a076d21
innov8ive <sanjayg312@gmail.com>
2024-03-27 16:03:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-upi://pay?pa=9764281607@upi&pn=Nirmala+Gupta+Test&tid=O5ABSkj84y&am=1&cu=INR&tn=Test+Note&tr=LBtwE92dXX
+
+let extras = {
+'method': 'get',
+'params': {
+	'cid': "5BH5FIMSITMZ0ZJ5V8BS8V093L702DNX",
+	'appId': "VA89480XOUGMECS1MU56"
+    }
+};
+manager.showFabButton(false);
+const data = await manager.showOtplessLoginPage(extras);
+
+let message: string = '';
+if (data.data === null || data.data === undefined) {
+message = data.errorMessage;
+} else {
+message ="token: ${data.data.token}";
+// todo here
+}
+VA89480XOUGMECS1MU56
+5BH5FIMSITMZ0ZJ5V8BS8V093L702DNX
+sb7muh67z6d8vcxgq6ynvg4zqrzyu89f
+
